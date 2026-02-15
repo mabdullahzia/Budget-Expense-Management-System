06faae124d53a635326afd5da92c9510af5939f6
Muhammad Abdullah Zia <abdullah2006zia@gmail.com>
2026-02-15 12:45:19 +0500

    Budget.h

--- Budget.h
+++ Budget.h
@@ -5,26 +5,45 @@
 using namespace std;
 
 class Income{
-public:
+private:
+    string source;
     float amount;
-    Income(float a){amount=a;}
+public:
+    Income(string s,float a){source=s;amount=a;}
+    string getSource(){return source;}
+    float getAmount(){return amount;}
+    void setAmount(float a){amount=a;}
 };
 
 class Expense{
-public:
+private:
     string category;
     float amount;
+public:
     Expense(string c,float a){category=c;amount=a;}
+    string getCategory(){return category;}
+    float getAmount(){return amount;}
+    void setAmount(float a){amount=a;}
 };
 
 class Budget{
 private:
     vector<Income> incomes;
     vector<Expense> expenses;
+    float monthlyGoal;
 public:
-    void addIncome(float a);
+    Budget(){monthlyGoal=0;}
+    void addIncome(string s,float a);
+    void updateIncome(string s,float a);
+    void removeIncome(string s);
     void addExpense(string c,float a);
+    void updateExpense(string c,float a);
+    void removeExpense(string c);
     void displaySummary();
+    void displayCategorySummary();
+    void setMonthlyGoal(float g){monthlyGoal=g;}
+    void checkBudget();
+    void searchExpenses(string c);
 };
 
 #endif
